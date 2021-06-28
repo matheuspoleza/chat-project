a08c826e9b42b614a641e43f64a7f3f9ec09a923
Matheus Poleza <matheus.poleza@gmail.com>
2021-06-28 11:05:22 -0300

    feat(screens/Chat): creates first chat version integrating with state api

--- src/screens/Chat/Chat.tsx
+++ src/screens/Chat/Chat.tsx
@@ -1,26 +1,61 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { RouteComponentProps } from 'react-router';
 
 import stateApi from '../../api/stateApi';
 
-const Chat: React.FC = () => {
+interface Props {
+  userID: string;
+}
+
+const Chat: React.FC<RouteComponentProps<Props>> = ({ match }) => {
+  const { userID } = match.params;
+  const [messages, setMessages] = useState<string[]>([]);
+  const inputEl = useRef<HTMLInputElement>(null);
+
+  const handleSend = async (event: React.FormEvent) => {
+    event.preventDefault();
+
+    if (inputEl.current) {
+      const message = inputEl.current.value;
+      const newMessages = [];
+      const response = await stateApi.interact(message, userID);
+
+      for (let i = 0; i < response.length; i++) {
+        const responseMessage = response[i];
+        if (responseMessage.type === 'end') {
+          setMessages([]);
+          return;
+        }
+
+        if (responseMessage.type === 'speak') {
+          newMessages.push(responseMessage.payload.message);
+        }
+      }
+
+      setMessages([...messages, message, ...newMessages]);
+    }
+  };
+
   useEffect(() => {
-    stateApi.interact('', 'luke');
-  }, []);
+    if (inputEl.current) {
+      inputEl.current.value = '';
+    }
+  }, [messages]);
 
   return (
     <div>
-      <h1>Luke Chat</h1>
+      <h1>{userID} Chat</h1>
 
       <dl>
-        <dt>Can I order some pizza</dt>
-        <dd>Sure what kind of pizza do you want?</dd>
-
-        <dt>Pepperoni and Cheese</dt>
-        <dd>Great, pepperoni and cheese coming up!</dd>
+        {messages.map((message, idx) => {
+          return <dd key={idx}>{message}</dd>;
+        })}
       </dl>
 
-      <input placeholder="user input here" />
-      <button>send</button>
+      <form onSubmit={handleSend}>
+        <input ref={inputEl} placeholder="user input here" />
+        <button type="submit">send</button>
+      </form>
     </div>
   );
 };
