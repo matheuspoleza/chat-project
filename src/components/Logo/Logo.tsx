50ab1de34c591a9d870c4eac46f5d67c0475f8db
Matheus Poleza <matheus.poleza@gmail.com>
2021-06-29 01:49:42 -0300

    fix(components/Logo): adds voiceflow logo back again

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -12,10 +12,25 @@ const LogoImage = styled.div`
   justify-content: center;
   margin-right: 16px;
   color: white;
+
+  svg {
+    width: 16px;
+    height: 16px;
+  }
 `;
 
 const Logo: React.FC = () => {
-  return <LogoImage>P</LogoImage>;
+  return (
+    <LogoImage>
+      <svg viewBox="0 0 20 24" xmlns="http://www.w3.org/2000/svg">
+        <path
+          d="M14.136.816c1.006.57 2.488 2.587 1.906 6.935-.954 6.934-4.765 12.626-6.036 12.626-.74 0-.689-1.083.265-5.123 1.27-5.278 3.018-12.782-3.865-12.782-7.307 0-8.207 7.659-3.495 8.59-.688-3.57 1.483-6.934 3.547-6.934 2.224 0 1.271 4.192.37 8.695C5.769 18.049 4.552 24 8.788 24c5.876 0 10.271-10.609 11.065-16.253 1.01-7.138-3.439-9.053-5.717-6.93z"
+          fill="currentColor"
+          fillRule="evenodd"
+        ></path>
+      </svg>
+    </LogoImage>
+  );
 };
 
 export default Logo;
