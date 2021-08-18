823273b4d91fb19b22b9f9c02d032d656583d3d1
Matheus Poleza <matheus.poleza@voiceflow.com>
2021-08-18 11:01:32 -0300

    feat: adds faker

--- cypress/support/pages/ChatPage.js
+++ cypress/support/pages/ChatPage.js
@@ -2,14 +2,23 @@ import ChatElements from '../elements/ChatElements';
 import DashboardPage from './DashboardPage';
 
 const ChatPage = {
-  startChatSection: () => {
+  startChatSection: (userName) => {
     DashboardPage.accessSite();
-    DashboardPage.fillsUserName();
-    DashboardPage.startSection();
+    DashboardPage.fillsUserName(userName);
+    DashboardPage.submitsUser();
+    DashboardPage.startSection(userName);
   },
   sendMessage: (message) => {
     cy.getBySel(ChatElements.chatInput()).type(message);
     cy.getBySel(ChatElements.chatSubmitButton()).click();
+  },
+  waitForWelcomeMessage: () => {
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.wait(2000);
+  },
+  waitForResponseMessage: () => {
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.wait(100);
   },
 };
 
--- cypress/support/steps/ChatSteps.js
+++ cypress/support/steps/ChatSteps.js
@@ -1,15 +1,28 @@
 /* global Given, Then, When */
 
+import faker from 'faker';
+
 import ChatPage from '../pages/ChatPage';
 
 Given('I start a chat section', () => {
-  ChatPage.startChatSection('Matheus');
+  const userName = faker.name.firstName();
+  ChatPage.startChatSection(userName);
 });
 
 When('I answer all order questions', () => {
-  ChatPage.sendMessage('Hello world');
+  ChatPage.waitForWelcomeMessage();
+  ChatPage.sendMessage('I want a pizza');
+
+  ChatPage.waitForResponseMessage();
+  ChatPage.sendMessage('medium');
+
+  ChatPage.sendMessage('delivery');
+  ChatPage.waitForResponseMessage();
+
+  ChatPage.sendMessage('coke');
+  ChatPage.waitForResponseMessage();
 });
 
 Then('I see a thank you message with my order information', () => {
-  ChatPage.sendMessage('Hello world');
+  cy.contains('Great, your medium pizza will be delivered to you in 20 minutes.');
 });
